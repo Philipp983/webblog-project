a434a7e5f097c3cf9b1246e669ed2f77d70edcb0
Philipp987 <philipp-wagner91@web.de>
2023-08-21 22:16:19 +0200

    added the functionality to click on a "add comment" button, be redirected to another page to add comment on the chosen blogentry

--- src/main/java/de/brightslearning/webblog/comment/CommentController.java
+++ src/main/java/de/brightslearning/webblog/comment/CommentController.java
@@ -1,8 +1,44 @@
 package de.brightslearning.webblog.comment;
 
+import de.brightslearning.webblog.blogentry.BlogEntry;
+import de.brightslearning.webblog.blogentry.BlogEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CommentController {
 
+    @Autowired
+    private BlogEntryRepository blogEntryRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @GetMapping("/comment/{id}")
+    public String showCommentPage(@PathVariable Integer id, Model model) {
+        // Find the blog entry by ID
+        BlogEntry entry = blogEntryRepository.findById(id).orElse(null);
+
+        if (entry == null) {
+            // Handle the case when the entry is not found, e.g., redirect to an error page
+            return "error";
+        }
+
+        model.addAttribute("blogEntry", entry);
+        model.addAttribute("comment", new Comment()); // Add an empty comment to the model
+
+        return "comment"; // Name of your comment page HTML (e.g., comment.html)
+    }
+
+    @PostMapping("/comment/{id}")
+    public String addComment(@PathVariable Integer id, @ModelAttribute("comment") Comment comment, Model model) {
+        // Similar to above, you can retrieve the blog entry and save the comment
+        // Redirect to a success page or back to the blog entry page
+        return "redirect:/";
+    }
 }
