42dc77eaeb83b346693ba1a053eaf2b0fa8b7015
Philipp987 <philipp-wagner91@web.de>
2023-08-21 15:55:07 +0200

    Created admin account

--- src/main/java/de/brightslearning/webblog/HomeController.java
+++ src/main/java/de/brightslearning/webblog/HomeController.java
@@ -2,6 +2,8 @@ package de.brightslearning.webblog;
 
 import de.brightslearning.webblog.comment.CommentRepository;
 import de.brightslearning.webblog.user.BlogUser;
+import de.brightslearning.webblog.user.BlogUserRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,10 +14,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class HomeController {
     private final CommentRepository commentRepository;
 
-    @Autowired
-    public HomeController(CommentRepository commentRepository) {
-        this.commentRepository = commentRepository;
+    private final BlogUserRepository blogUserRepository;
+
+    @PostConstruct
+    public void generateDummyData() {
+        BlogUser admin = new BlogUser("admin", "admin", true);
+
+        blogUserRepository.save(admin);
     }
+    @Autowired
+    public HomeController(CommentRepository commentRepository, BlogUserRepository blogUserRepository) {
+        this.commentRepository = commentRepository;
+        this.blogUserRepository = blogUserRepository;
+    }
+
+
+
 
     @GetMapping("/")
     public String home(@ModelAttribute("sessionUser") BlogUser sessionUser, Model model) {
