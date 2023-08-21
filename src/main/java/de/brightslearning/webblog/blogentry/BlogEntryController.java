48a322bebeb70e033f1565a5dff44b7890071930
Philipp987 <philipp-wagner91@web.de>
2023-08-21 21:35:37 +0200

    fetched the user_id and saved it with the blog_entry...for later I suppose?

--- src/main/java/de/brightslearning/webblog/blogentry/BlogEntryController.java
+++ src/main/java/de/brightslearning/webblog/blogentry/BlogEntryController.java
@@ -1,5 +1,6 @@
 package de.brightslearning.webblog.blogentry;
 
+import de.brightslearning.webblog.user.BlogUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,9 +27,21 @@ public class BlogEntryController {
     }
 
     @PostMapping(value ="/message")
-    public String addEntry(@ModelAttribute("BlogEntry")BlogEntry entry) {
+    public String addEntry(@ModelAttribute("BlogEntry")BlogEntry entry, Model model) {
+
+        BlogUser currentUser = (BlogUser) model.getAttribute("sessionUser");
+        if(currentUser == null) {
+            // The user is not logged in or the session expired. Handle this case appropriately.
+            // Maybe redirect to the login page or show an error message.
+            return "redirect:/login";
+        }
+        entry.setBlogUser(currentUser);
+
         entry.setDate(LocalDateTime.now());
+
         blogEntryRepository.save(entry);
+
+
         return "redirect:/";
     }
 }
