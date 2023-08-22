d4a2e1eafe5a4d7bb164eae6e4270b77fba3a487
Philipp987 <philipp-wagner91@web.de>
2023-08-22 09:18:48 +0200

    fixed the null value for comment, added mapping of blog_user_id to comment

--- src/main/java/de/brightslearning/webblog/comment/CommentController.java
+++ src/main/java/de/brightslearning/webblog/comment/CommentController.java
@@ -2,6 +2,7 @@ package de.brightslearning.webblog.comment;
 
 import de.brightslearning.webblog.blogentry.BlogEntry;
 import de.brightslearning.webblog.blogentry.BlogEntryRepository;
+import de.brightslearning.webblog.user.BlogUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -39,12 +40,17 @@ public class CommentController {
     @PostMapping("/comment/{id}")
     public String addComment(@PathVariable Integer id, @ModelAttribute("comment") Comment comment, Model model) {
         BlogEntry entry = blogEntryRepository.findById(id).orElse(null);
-
+        BlogUser currentUser = (BlogUser) model.getAttribute("sessionUser");
+        if(currentUser == null) {
+            // The user is not logged in or the session expired. Handle this case appropriately.
+            // Maybe redirect to the login page or show an error message.
+            return "redirect:/login";
+        }
         if (entry == null) {
             // Handle the case when the entry is not found
             return "error";
         }
-
+        comment.setBlogUser(currentUser);
         comment.setBlogEntry(entry); // Associate the comment with the blog entry
 
         commentRepository.save(comment); // Save the comment to the database
